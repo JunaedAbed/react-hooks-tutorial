c356d894b1563028b6d8fb6cd4bdc0d03f063ba6
Junaed Abed <junaedccpc17@gmail.com>
2021-02-04 01:09:48 +0600

    useState tut

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { useForm } from './useForm';
 
 function App() {
+  
+  const [values, handleChange] = useForm({email: '', password: ''});
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <div>heyyyyyy</div>
+        
+        <div>
+          <div><input name='email' placeholder='email' value={values.email} onChange={handleChange} /></div>
+          <div><input name='password' type='password' placeholder='password' value={values.password} onChange={handleChange} /></div>
+        </div>
+        
       </header>
     </div>
   );
