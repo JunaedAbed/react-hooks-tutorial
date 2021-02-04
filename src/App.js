477e6d3a069de03b7ffc582586f19da630a523c7
Junaed Abed <junaedccpc17@gmail.com>
2021-02-04 20:42:57 +0600

    useEffect tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { useFetch } from './useFetch';
 import { useForm } from './useForm';
 
 function App() {
   
-  const [values, handleChange] = useForm({email: '', password: ''});
+  const [values, handleChange] = useForm({email: '', password: '', firstName: ''});
   
+  const [count, setCount] = useState(() => 
+    JSON.parse(localStorage.getItem('count'))//init function
+  );
+  const {data, loading} = useFetch(`http://numbersapi.com/${count}/math`);
+  
+  
+  useEffect(() => {
+    localStorage.setItem('count', JSON.stringify(count));//set count in local storage
+  }, [count]);
+  
+  //http://numbersapi.com/5/math
+  
+  // useEffect(() => {
+  //   const onMouseMove = e => {
+  //     console.log(e);
+  //   }
+  //   window.addEventListener('mousemove', onMouseMove);//when mounts
+    
+  //   return () => {
+  //     window.removeEventListener('mousemove', onMouseMove); //when unmounts
+  //   }
+  // }, []);
+  
+  // useEffect(() => {
+  //   console.log('mount1');
+  // })
+  
+  // useEffect(() => {
+  //   console.log('mount2');
+  // })
   
   return (
     <div className="App">
       <header className="App-header">
         
         <div>
+          <div>{!data ? 'loading...' : data}</div>
+          <div>count: {count}</div>
+          <button onClick={() => setCount(c => c+1)}>increment</button>
           <div><input name='email' placeholder='email' value={values.email} onChange={handleChange} /></div>
+          <div><input name='firstName' placeholder='fName' value={values.firstName} onChange={handleChange} /></div>
           <div><input name='password' type='password' placeholder='password' value={values.password} onChange={handleChange} /></div>
         </div>
         
