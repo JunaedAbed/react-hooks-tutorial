eb9e11fc21c1d052621d51b0cbb20be78968e8ab
Junaed Abed <junaedccpc17@gmail.com>
2021-02-07 22:22:01 +0600

    unfinished useContext

--- src/App.js
+++ src/App.js
@@ -1,65 +1,37 @@
-//useReducer is for storing state. alt to useState hook.
-//for simpler things should use useState.
+//useContext hook is used to store user and share it.
 
-import React, { useReducer, useState } from 'react';
+import React, {} from 'react';
 import './App.css';
-
-function reducer(state, action){
-  switch (action.type) {
-    case 'add-todo':
-      return {
-        todos: [...state.todos, {text: action.text, completed: false}],
-        todoCount: state.todoCount + 1
-      };
-    case 'toggle-todo':
-      return {
-        todos: state.todos.map((t, idx) => 
-          idx === action.idx ? {...t, completed: !t.completed} : t
-        ),//iterates through the list and flip completed at a certain index.
-        todoCount: state.todoCount
-      };
-   
-    default:
-      return state;
-  }
-}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {Index} from './pages/index';
+import {About} from './pages/about';
 
 function App() {
-  
-  const [{todos, todoCount}, dispatch] = useReducer(reducer, {
-    todos : [], 
-    todoCount: 0
-  });
-  const [text, setText] = useState();
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Todo-List</h1>
-        <div>
-          <form onSubmit={e => {
-            e.preventDefault();
-            dispatch({type: 'add-todo', text});
-            setText(''); //this will clear out the text
-          }}>
-            <input value={text} onChange={e => setText(e.target.value)} placeholder="add todos" />  
-          </form>
-          <div>number of todos: {todoCount}</div>
-          {todos.map((t, idx) => (
-            <div 
-              key={t.text} 
-              onClick={() => dispatch({type: 'toggle-todo', idx})}
-              style={{
-                textDecoration: t.completed ? 'line-through' : ''
-              }}
-            >
-              {t.text}
-            </div>
-          ))}
-        </div>
+    
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
         
-      </header>
-    </div>
+        <Route path='/' exact component={Index}></Route>
+        <Route path='/about/' component={About}></Route>
+      </div>
+    </Router>
+     
   );
 }
 
