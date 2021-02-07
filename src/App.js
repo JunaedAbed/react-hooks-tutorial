1de4de24fd1b67b9490c9f58cad297a52bf3f386
Junaed Abed <junaedccpc17@gmail.com>
2021-02-07 21:05:55 +0600

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-//useCallback function is used to prevent functions being created on every single render.
+//useMemo hook is for optimizing computed values.
 
-import React, { useCallback, useState } from 'react';
+import React, { useMemo, useState } from 'react';
+import { useFetch } from './useFetch';
+
 import './App.css';
-import { Hello } from './Components/Hello';
-import { Square } from './Components/Square';
+
 
 
 function App() {
   
   const [count, setCount] = useState(0);
+  const {data} = useFetch('https://raw.githubusercontent.com/ajzbc/kanye.rest/quotes/quotes.json');
   
-  const fvrtNums = [7, 21, 37];
-  
-  const increment = useCallback((n) => {
-    setCount(c => c + n);
-  
-  }, [ setCount]);
-  // const increment = useCallback(() => {
-  //   setCount(c => c + 1);
-  // }, [ setCount]);
-  
-  //when count or setCount changes, this func will be recreated and will be put in increment var.
+  const longestWord = useMemo(() => computeLongestWord(data), [data, computeLongestWord])
 
   return (
     <div className="App">
       <header className="App-header">
         
         <div>
-          <Hello increment={increment} />
           <div>Count: {count}</div>
-          {fvrtNums.map(n => {
-            return (
-              <Square increment={increment} n={n} key={n} />
-            );
-          })}
+          <button onClick={() => setCount(count + 1)}>increment</button>
+          <div>{longestWord}</div>
         </div>
         
       </header>
     </div>
   );
 }
+
+function computeLongestWord(arr) {
+    if(!arr){
+      return [];
+    }
+    
+    console.log('computing longest word');
+    let longestWord = '';
+    
+    JSON.parse(arr).forEach(sentence => sentence.split(' ').forEach(word => {
+      if(word.length > longestWord.length){
+        longestWord = word;
+      }
+    }));
+    
+    return longestWord;
+  }
 
 export default App;
